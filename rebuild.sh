795171b29231fa95a8b9d383e5417b779a763c85
Luis Abreu <lmjabreu@gmail.com>
2011-02-21 16:48:33 +0000

    deleted rebuild.sh

--- rebuild.sh
+++
@@ -1,7 +0,0 @@
-sudo /usr/bin/php reset-all.php ; \
-/usr/bin/php twitter-search.php ; \
-sudo /usr/bin/php make-images.php ; \
-sudo /usr/bin/php make-images.php ; \
-sudo /usr/bin/php make-images.php ; \
-sudo /usr/bin/php make-images.php ; \
-sudo /usr/bin/php collage-build.php
