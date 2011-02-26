ee2a2790d44239dfd75900dd7b807ecd3ebe5f9a
Leonardo Xavier <leo@skipper.lan>
2011-02-26 00:10:21 +0000

    Complete general.js rewrite, some HTML tweaks on index.php

--- public/assets/js/general.js
+++ public/assets/js/general.js
@@ -1,134 +1,84 @@
-  var idx = 0;
-	var party = {
-    canvas: null,
-    cols: 48,
-    rows: 47,
-    tile_size: 12,
-    performance_mode: false,
-	  grid: [],
-	  index: [],
-	  tweets: [],
-	  tweetsCount: [],
-	  buffer: [],
-	  bufferCount: 0
-  }
-  
-  function finish() {
-    $('div.tile').tipsy();
-  }
-  
-  function addImage() {
-    var tweet = party.tweets[idx],
+var party = party || {};
+
+(function () {
+	
+	// Build the Initial Mosaic
+	function initialBuild() {
+		var frame_interval,
+			tile_counter = 0,
+			frame_counter = 0;
+		// Start the recursive call for each frame
+		frame_interval = setInterval(initialBuildFrame, (1000/frames_per_second) );
+	}
+	
+	function initialBuildFrame() {
+		
+		var tiles_to_draw = "",
+			tweets = party.tweets,
+			tweet,
       position,
       index,
       x = 0,
       y = 0;
-    
-    idx += 1;
-    
-    // Make sure this is an existing data entry
-	  if (!tweet) {
-		  return;
-	  }
-	  // Cache the tweets position
-	  position = tweet.position;
-	  index = party.index[position];
-	  
-	  if (!index) {
-		  return;
-	  }
-	  
-    x = index.x * 12;
-    y = index.y * 12;
-	  
-	  party.buffer.push('<div class="tile" style="background-image:url(data:image/jpg;base64,' + tweet.imageData + '); left: ' + x + 'px; top: ' + y + 'px;" original-title="' + tweet.contents + '"></div>');
- 		party.bufferCount += 1;
- 		
-    if (party.bufferCount >= 20) {
-      addBufferToDom();
-    }
-    
-    // Is this the end of the line?
-    if (idx == party.tweetsCount) {
-      addBufferToDom();
-      finish();
-    }
-  }
-  
-  function addBufferToDom() {
-    party.canvas.append(party.buffer.join(' '));
-    party.bufferCount = 0;
-    party.buffer = [];
-  }
-  
-  $(document).ready(function() {
-    // cache dom element
-    party.performance_mode = $.browser.msie;
-	  party.canvas = $('#mosaic');
+
+		// Build tiles_per_frame tiles and draw them
+		for (; tile_counter < (tile_counter + tiles_per_frame); tile_counter += 1) {
+			
+			// Make sure this is an existing data entry
+			tweet = tweets[tile_counter];
+		  if (!tweet) {
+			  continue;
+		  }
+		
+		  // Cache the tweets position
+		  position = tweet.position;
+		  index = party.index[position];
+		  if (!index) {
+			  continue;
+		  }
+
+			// Calculate top/left position of the tile
+	    x = index.x * party.tile_size;
+	    y = index.y * party.tile_size;
+
+			// Add it to the HTML to draw
+		  tiles_to_draw = '<div class="tile" style="background-image:url(data:image/jpg;base64,' + tweet.imageData + '); left: ' + x + 'px; top: ' + y + 'px;" original-title="' + tweet.contents + '"></div>';
+		
+		}
+		
+		if (tiles_to_draw) {
+			// Draw the tiles and proceed
+			party.canvas.append(tiles_to_draw);
+			// Another frame completed
+			frame_counter += 1;
+		} else {
+			// No Tiles were built - task is complete
+			clearInterval(frame_interval);
+		}
+		
+	}
 	
-	  function start() {
-	  
-		  var funcArr = [];
-      
-      for (var x = 0; x < (party.tweetsCount/10); x += 1) {
-        funcArr.push(function(){
-          addImage();
-		      addImage();
-		      addImage();
-		      addImage();
-		      addImage();
-		      addImage();
-		      addImage();
-		      addImage();
-		      addImage();
-		      addImage();
-          addImage();
-		      addImage();
-		      addImage();
-		      addImage();
-		      addImage();
-		      addImage();
-		      addImage();
-		      addImage();
-		      addImage();
-		      addImage();
-        });
-	    }
-      multiStep(funcArr, 20);
-    }
-
-    /*
-    * Multistep task processing
-    *
-    * @param Array     steps
-    * @param Array     args
-    * @param Function  callback
-    * @param int       interval
-    */
-    function multiStep(steps, interval, args, callback) {
-      var tasks = steps.concat(), //clone array
-        task;
-      setTimeout(function(){
-        //execute next task
-        task = tasks.shift();
-        if (task && typeof task == 'function') {
-          task.apply(null, args || []);
-        }
-
-        //determine if there's more tasks
-        if (tasks.length > 0) {
-          setTimeout(arguments.callee, interval);
-        } else {
-          if (typeof callback ==='function') {
-            callback();
-          }
-        }
-      }, interval);
-    }
-
-    $.getJSON('page.php', function(data) {
-      party.tweets = data.payload.tweets;
-      party.tweetsCount = party.tweets.length;
-		  start();
-    });
+	$.extend(party, {
+		frames_per_second: 12,
+		tiles_per_frame: 20,
+		cols: 48,
+		rows: 47,
+		tile_size: 12,
+		grid: [],
+		index: []
 	});
+	
+}());
+
+
+$(document).ready(function() {
+	// cache dom element
+	party.performance_mode = $.browser.msie;
+	party.canvas = $('#mosaic');
+	
+	$.getJSON('page.php', function(data) {
+		party.tweets = data.payload.tweets;
+		start();
+	});
+	
+});
