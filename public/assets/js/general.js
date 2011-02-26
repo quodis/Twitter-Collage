721bbf4c98179e40f93ef69b697b4e02f81a4ec3
Leonardo Xavier <leo@skipper.lan>
2011-02-26 01:11:24 +0000

    minor general.js changes

--- public/assets/js/general.js
+++ public/assets/js/general.js
@@ -1,13 +1,13 @@
 var party = party || {};
 
 (function () {
-	
+	var frame_interval,
+		tile_counter = 0,
+		frame_counter = 0;
+		
 	// Build the Initial Mosaic
 	function initialBuild() {
-		var frame_interval,
-			tile_counter = 0,
-			frame_counter = 0;
-			
+		
 		// Start the recursive call for each frame
 		frame_interval = setInterval(initialBuildFrame, (1000/party.initial.frames_per_second) );
 		
@@ -22,38 +22,42 @@ var party = party || {};
       position,
       index,
       x = 0,
-      y = 0;
-
+      y = 0,
+			i = 0,
+			j = (tile_counter + party.initial.tiles_per_frame);
+		
 		// Build tiles_per_frame tiles and draw them
-		for (; tile_counter < (tile_counter + party.initial.tiles_per_frame); tile_counter += 1) {
+		for (i = tile_counter; i < j; i += 1) {
 			
 			// Make sure this is an existing data entry
-			tweet = tweets[tile_counter];
-		  if (!tweet) {
+			tweet = tweets[i];
+			if (!tweet) {
 			  continue;
-		  }
-		
-		  // Cache the tweets position
-		  position = tweet.position;
-		  index = party.index[position];
-		  if (!index) {
+			}
+			
+			// Cache the tweets position
+			position = tweet.position;
+			index = party.index[position];
+			if (!index) {
 			  continue;
-		  }
-
+			}
+			
 			// Calculate top/left position of the tile
-	    x = index.x * party.tile_size;
-	    y = index.y * party.tile_size;
-
+			x = index.x * party.tile_size;
+			y = index.y * party.tile_size;
+			
 			// Add it to the HTML to draw
-		  tiles_to_draw = '<div class="tile" style="background-image:url(data:image/gif;base64,' + tweet.imageData + '); left: ' + x + 'px; top: ' + y + 'px;" original-title="' + tweet.contents + '"></div>';
-		
+			tiles_to_draw = tiles_to_draw + '<div class="tile" style="background-image:url(data:image/gif;base64,' + tweet.imageData + '); left: ' + x + 'px; top: ' + y + 'px;" original-title="' + tweet.contents + '"></div>';
+
 		}
+		tile_counter = i;
 		
 		// Check if anything to draw was processed
 		if (tiles_to_draw) {
-			
+
 			// Draw the tiles and proceed
 			party.canvas.append(tiles_to_draw);
+			
 			// Another frame completed
 			frame_counter += 1;
 			
@@ -67,17 +71,17 @@ var party = party || {};
 	}
 	
 	function showLoading() {
-		console.log('Loading');
+		
 	}
 	
 	function hideLoading() {
-		console.log('Stopped loading');
+
 	}
 	
 	$.extend(party, {
 		"initial": {
-			"frames_per_second": 12,
-			"tiles_per_frame": 20,
+			"frames_per_second": 24,
+			"tiles_per_frame": 10,
 			"build": initialBuild,
 			"loadingMessages": [
 				"Sorting guest list alphabetically",
@@ -85,7 +89,7 @@ var party = party || {};
 				"Randomnizing seating-order",
 				"Syncing disco-lights to the beat",
 				"Cooling drinks to ideal temperature",
-				"Handing out name-tags" ]
+				"Handing out name-tags" ],
 			"showLoading": showLoading,
 			"hideLoading": hideLoading
 		},
