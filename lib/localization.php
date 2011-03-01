5fe6281710d1070bbe4937fdad14c95025ea1ffb
Luis Abreu <lmjabreu@gmail.com>
2011-03-01 01:00:33 +0000

    fixed language_code normalization redirect loop

--- lib/localization.php
+++ lib/localization.php
@@ -16,7 +16,7 @@ $regex_pattern = "/([a-z]{2}-)([a-z]{2})/is";
 // parse the accept language header
 preg_match_all($regex_pattern, $_SERVER['HTTP_ACCEPT_LANGUAGE'], $accept_language);
 // if there's no language code in the request_uri
-if ($_SERVER['REQUEST_URI'] == '/' || ctype_lower($accept_language[2][0]))
+if ($_SERVER['REQUEST_URI'] == '/')
 	// redirect the user to his preferred language as defined in the accept-language request header
 	header('Location: /' . $accept_language[1][0] . strtoupper($accept_language[2][0]));
 
@@ -25,6 +25,12 @@ bindtextdomain("all", dirname(__FILE__) . "/../locale");
 
 // find the language code in the REQUEST_URI
 preg_match_all($regex_pattern, $_SERVER['REQUEST_URI'], $request_uri);
+// normalize request URI
+// check if the second fragment is lowercase
+if ($_SERVER['REQUEST_URI'] != '/' && ctype_lower($request_uri[2][0]))
+	// redirect the user to /xx-XX
+	header('Location: /' . $request_uri[1][0] . strtoupper($request_uri[2][0]));
+
 // language code
 $language_code = str_replace('-', '_', $request_uri[1][0]) . strtoupper($request_uri[2][0]);
 // set the locale
