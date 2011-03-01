f629cd9374b89d6e276de3ebcb6b26d36c81f117
Luis Abreu <lmjabreu@gmail.com>
2011-03-01 00:17:09 +0000

    tweaked language code regex

--- lib/localization.php
+++ lib/localization.php
@@ -10,20 +10,25 @@
  * @subpackage	server
  **/
 
+// define the regex_pattern to catch langcodes
+$regex_pattern = "/.*?([a-z]{2}-)([a-zA-Z]{2})/is";
+
 // parse the accept language header
-preg_match_all('/([a-z]{1,8}(-[a-z]{1,8})?)\s*(;\s*q\s*=\s*(1|0\.[0-9]+))?/i', $_SERVER['HTTP_ACCEPT_LANGUAGE'], $accept_language);
+preg_match_all($regex_pattern, $_SERVER['HTTP_ACCEPT_LANGUAGE'], $accept_language);
 // if there's no language code in the request_uri
 if ($_SERVER['REQUEST_URI'] == '/')
 	// redirect the user to his preferred language as define in the accept-language browser header
-	header('Location: /' . $accept_language[1][0]);
+	header('Location: /' . $accept_language[1][0] . strtoupper($request_uri[2][0]));
 
 // define where the translation files are stored in disk
 bindtextdomain("all", dirname(__FILE__) . "/../locale");
 
 // find the language code in the REQUEST_URI
-preg_match_all("/.*?([a-z]{2}-[A-Z]{2})/is", $_SERVER['REQUEST_URI'], $request_uri);
+preg_match_all($regex_pattern, $_SERVER['REQUEST_URI'], $request_uri);
+// language code
+$language_code = str_replace('-', '_', $request_uri[1][0]) . strtoupper($request_uri[2][0]);
 // set the locale
-$locale = str_replace('-', '_', $request_uri[1][0]) . '.utf8';
+$locale = $language_code . '.utf8';
 putenv("LC_ALL=$locale");
 setlocale(LC_ALL, $locale);
 textdomain("all");
