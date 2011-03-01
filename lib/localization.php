9c9333bc8c832a9b9a68a9332ce4c661d1e5e6cb
Luis Abreu <lmjabreu@gmail.com>
2011-03-01 00:21:25 +0000

    more regex tweaks

--- lib/localization.php
+++ lib/localization.php
@@ -11,14 +11,14 @@
  **/
 
 // define the regex_pattern to catch langcodes
-$regex_pattern = "/.*?([a-z]{2}-)([a-zA-Z]{2})/is";
+$regex_pattern = "/([a-z]{2}-)([a-z]{2})/is";
 
 // parse the accept language header
 preg_match_all($regex_pattern, $_SERVER['HTTP_ACCEPT_LANGUAGE'], $accept_language);
 // if there's no language code in the request_uri
 if ($_SERVER['REQUEST_URI'] == '/')
 	// redirect the user to his preferred language as define in the accept-language browser header
-	header('Location: /' . $accept_language[1][0] . strtoupper($request_uri[2][0]));
+	header('Location: /' . $accept_language[1][0] . strtoupper($accept_language[2][0]));
 
 // define where the translation files are stored in disk
 bindtextdomain("all", dirname(__FILE__) . "/../locale");
