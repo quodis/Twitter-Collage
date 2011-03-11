3ffd9bb4c0482405d4f56c9e5c2e1656190fa8ad
Leonardo Xavier <leo@skipper.lan>
2011-03-11 17:27:04 +0000

    minor changes

--- public/assets/js/general.js
+++ public/assets/js/general.js
@@ -177,9 +177,10 @@ var party = party || {};
 			// No Tiles were built - task is complete
 			window.clearInterval(initial_draw_timer);
 			// Remove the grid
-			//party.canvas.css('background', 'none');
+			party.canvas.css('background', 'none');
 			// Set counter to last id
 			counter.current = parseInt(state.total_tiles, 10);
+			alert('gi');
 			setCounter();
 			startAutoBubble();
 			startDrawNewTiles();
