3f72078b8a9d14409e6805f2932828f6bc966631
Leonardo Xavier <leo@skipper.lan>
2011-03-12 02:47:05 +0000

    Added target="_blank" to all links on the tweet bubbles

--- public/index.php
+++ public/index.php
@@ -167,12 +167,12 @@ function main()
 
 						<header>
 
-							<?php $link = '<a href="#" title="' . _('Twitter profile') . '" rel="author external"></a>'; ?>
+							<?php $link = '<a href="#" title="' . _('Twitter profile') . '" rel="author external" target="_blank"></a>'; ?>
 							<h1><?= sprintf(/* L10n: Used in: "twitter username" wrote */ _('%s <span>wrote</span>'), $link) ?></h1>
-							<a href="#" title="" rel="author external" class="twitter-avatar">
+							<a href="#" title="" rel="author external" class="twitter-avatar" target="_blank">
 								<img  src="data:image/gif;base64,R0lGODlhAQABAPAAAP8A/wAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="<?= _('Twitter profile picture') ?>" width="48" height="48" />
 							</a>
-							<time datetime="<?=date('Y-m-d')?>" pubdate><a href="#" rel="bookmark external" title="<?= _('Permalink') ?>"></a></time>
+							<time datetime="<?=date('Y-m-d')?>" pubdate><a href="#" rel="bookmark external" title="<?= _('Permalink') ?>" target="_blank"></a></time>
 
 						</header>
 
